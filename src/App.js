eeba899f0295c7321a907bbc8c9ff488ef58fda9
adamswain <avswain91@gmail.com>
2023-08-24 07:40:33 -0400

    moving project to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './CSS/App.css';
+import Header from './Components/Header.js';
+import Footer from './Components/Footer.js';
+import PostForm from './Components/PostForm.js';
+import PostList from './Components/PostList.js';
+import {useState} from "react";
+import StorePost from "./Components/StorePost";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const addPost = (newPost) => {
+      setPosts([...posts, newPost]);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Header />
+          <div className="container">
+              <PostForm addPost={addPost} />
+              <PostList posts={posts} />
+          </div>
+          <StorePost posts={posts} />
+        <Footer />
+      </div>
   );
 }
 
 export default App;
+
